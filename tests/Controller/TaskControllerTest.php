379e6ea9e325b4167b55a7e1c178da0e8ceec52f
CrabThug <damien.millet@nasm.fr>
2020-05-17 10:13:17 +0200

    :white_check_mark: Adding tests.

---
+++ tests/Controller/TaskControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\TaskController;
+use PHPUnit\Framework\TestCase;
+
+class TaskControllerTest extends TestCase
+{
+
+    public function testCreateAction()
+    {
+
+    }
+
+    public function testEditAction()
+    {
+
+    }
+}
